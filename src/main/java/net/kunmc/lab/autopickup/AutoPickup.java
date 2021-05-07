38a2c8c32d9f7289da0f862078be24970d5ca750
Kamesuta <kamesuta@gmail.com>
2021-05-08 06:44:32 +0900

    AutoPickup

---
+++ src/main/java/net/kunmc/lab/autopickup/AutoPickup.java
@@ -0,0 +1,46 @@
+package net.kunmc.lab.autopickup;
+
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+
+public final class AutoPickup extends JavaPlugin implements Listener {
+    private static class PlayerData {
+        public Block lastLookingBlock;
+    }
+
+    private Map<UUID, PlayerData> playerDataMap = new HashMap<>();
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        UUID uuid = player.getUniqueId();
+        PlayerData data = playerDataMap.computeIfAbsent(uuid, id -> new PlayerData());
+        Block lookingBlock = player.getTargetBlock(4);
+        if (lookingBlock != null && !Objects.equals(data.lastLookingBlock, lookingBlock)) {
+            data.lastLookingBlock = lookingBlock;
+            PlayerInventory inventory = player.getInventory();
+            lookingBlock.getDrops().forEach(inventory::addItem);
+        }
+    }
+}
